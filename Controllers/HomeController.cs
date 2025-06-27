3f7b6faa4b8da353bbf0344a2e22c98d0d63a735
lahiruomadhawa <lahirumadhawa00@gmail.com>
2025-06-28 05:05:12 +1000

    Controller and View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,54 @@
 using System.Diagnostics;
+using System.Text.Json;
 using k8sFormApp.Models;
+using k8sFormApp.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace k8sFormApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IRedisService _redisService;
+
+        public HomeController(IRedisService redisService)
         {
-            _logger = logger;
+            _redisService = redisService;
         }
 
         public IActionResult Index()
-        {
-            return View();
+        {            
+            return View(new Person());
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public async Task<IActionResult> Index(Person person)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var personJson = JsonSerializer.Serialize(new
+                    {
+                        FirstName = person.FirstName,
+                        LastName = person.LastName,
+                        Address = person.Address,
+                        CreatedAt = DateTime.UtcNow
+                    });
+                    await _redisService.AddPersonAsync(personJson);
+
+                    ViewBag.Message = "Person saved successfully!";
+                    ModelState.Clear(); // Clear the model state to reset the form
+                    return View(new Person()); // Return a new instance of Person to reset the form
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = $"Error saving person: {ex.Message}";
+                }
+            }
+            return View(person);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
 }
