52b024d5c01fb5b17823360e7dd97e42938573b0
lahiruomadhawa <lahirumadhawa00@gmail.com>
2025-06-28 04:41:42 +1000

    Initial commit (Project created)

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace k8sFormApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
